09878cb1f6ae65b19593f1a1347e0e53943eb9c1
Tony <158815401+frogCaller@users.noreply.github.com>
2025-10-01 18:58:33 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y fortune python3-pip python3-pil python3-numpy python3-spidev python3-smbus
+sudo apt-get install -y fortune python3-pip python3-pil python3-numpy python3-spidev python3-smbus python3-pygame
